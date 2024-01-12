f575bbfcea38ca05cf8d8839b34f456e3f4fb0d8
Abdelmoniem Ahmed Hgagy <137680518+Abdelmoniemhgagyy@users.noreply.github.com>
2024-01-12 14:59:11 +0200

    add moshaf

--- src/Pages/moshaf/Moshaf.jsx
+++ src/Pages/moshaf/Moshaf.jsx
@@ -19,23 +19,39 @@ function Moshaf() {
     <>
 
     <OneMoshaf Src={`https://app.quranflash.com/book/Medina3/epub/EPUB/imgs/`} 
+    name="المصحف الجوامعي"
     typeImg={`.png`}
     fahrs={622}
      />
     <OneMoshaf Src={`https://app.quranflash.com/book/Medina1/epub/EPUB/imgs/`} 
+    name="مصحف المدينة"
     typeImg={`.gif`}
     fahrs={622}
      />
 
     <OneMoshaf Src={`https://app.quranflash.com/book/Medina2/epub/EPUB/imgs/`} 
+    name=""
+    typeImg={`.png`}
+    fahrs={622}
+     />
+
+    <OneMoshaf Src={`https://app.quranflash.com/book/MedinaOld/epub/EPUB/imgs/`} 
+    name="مصحف المدينة القديم"
     typeImg={`.png`}
     fahrs={622}
      />
     <OneMoshaf Src={`https://app.quranflash.com/book/Shubah/epub/EPUB/imgs/`} 
+    name="رواية شعبة"
     typeImg={`.png`}
     fahrs={622}
      />
+    <OneMoshaf Src={`https://app.quranflash.com/book/Douri/epub/EPUB/imgs/`} 
+    name="مصحف برواية الدوري"
+    typeImg={`.png`}
+    fahrs={542}
+     />
     <OneMoshaf Src={`https://app.quranflash.com/book/Qaloon/epub/EPUB/imgs/`} 
+    name=""
     typeImg={`.png`}
     fahrs={574}
      />
