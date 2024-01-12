9d314d11eaaeda1dcfc9371f7530240a4465159e
Abdelmoniem Ahmed Hgagy <137680518+Abdelmoniemhgagyy@users.noreply.github.com>
2024-01-12 22:34:11 +0200

    add moshaf

--- src/Pages/moshaf/OneMoshaf.jsx
+++ src/Pages/moshaf/OneMoshaf.jsx
@@ -4,7 +4,8 @@ import  { useState } from "react"
 function OneMoshaf({Src,typeImg,name,fahrs}) {
 
   const [numberOfPage, setNumberOfPage] = useState(1);
-
+  const [openModel,setOpenModel] = useState(false)
+  
   const numberOfzerosUrl = numberOfPage < 10 ? `000`
   :numberOfPage <100 ? `00` 
   :`0`
@@ -37,9 +38,14 @@ const nextPage = ()=>{
     setNumberOfPage(1)
   }
 }
-
+useEffect(()=>{
+   window.scroll({
+    top:0,
+    behavior:'smooth'
+   })
+},[openModel])
 // End arrow icons functions 
-  return (
+return (
     <>
     <div  className='mt-[15px] mr-[60px] flex items-center justify-center flex-col'>
       <div className='my-4 md:my-2  md:mr-[42px] flex justify-center'>
@@ -54,7 +60,8 @@ const nextPage = ()=>{
       <i class="bi bi-arrow-right-circle sm:text-3xl text-xl cursor-pointer"  onClick={prevPage} ></i>
 
         <img className='w-[80%] sm:w-[70%] md:w-full md:h-[600px] rounded-[2px]'
-         src={`${Src}${numberOfzerosUrl}${numberOfPage}${typeImg}`} alt="moshaf" />
+         src={`${Src}${numberOfzerosUrl}${numberOfPage}${typeImg}`} alt="moshaf" 
+         onClick={()=>setOpenModel(!openModel)}/>
       
         <i class="bi bi-arrow-left-circle sm:text-3xl text-xl cursor-pointer"  onClick={nextPage}></i>
       </div>
@@ -62,7 +69,43 @@ const nextPage = ()=>{
 
     </div>
 
+ {/* model  */}
+    {openModel&&<div className='absolute top-0 left-0 min-w-full min-h-full bg-[#777] z-[99999999]' onClick={()=>setOpenModel(false)}>
+       
+       
+          <div className='relative mt-[140px] mt-[100px] flex gap-[2px] md:gap-[10px] items-center 
+          justify-center text-white pb-[3px]'
+          >
 
+             <i onClick={()=>setOpenModel(!openModel)}
+             class="bi bi-x-circle text-2xl md:text-3xl text-[white] hover:text-red-500 
+             absolute top-[-35px] left-[0x] md:left-[49%] md:top-[-50px] cursor-pointer"></i>
+
+             
+
+             <i class="bi bi-arrow-right-circle sm:text-3xl text-xl cursor-pointer"   
+                onClick={(e)=>{
+                e.stopPropagation();
+                prevPage()}}></i>
+
+             <img className='w-[80%]  md:w-[60%] xl:w-[40%] md:h-[600px] rounded-[2px]'
+             src={`${Src}${numberOfzerosUrl}${numberOfPage}${typeImg}`} alt="moshaf"
+              onClick={(e)=>e.stopPropagation()} />
+          
+            <i class="bi bi-arrow-left-circle sm:text-3xl text-xl cursor-pointer"  
+             onClick={(e)=>{
+              e.stopPropagation();
+               nextPage()}}></i>
+          </div>
+     <div className='text-center pb-[40px]'>
+     <input type="number" min={1} placeholder='رقم الصفحة' className=' p-2 w-[60%] md:w-auto rounded-lg border-none outline-none '
+          value={numberOfPage}
+          onChange={changeInputValue} />
+     </div>
+        
+      </div>}
+ {/* End model  */}
+ 
 
     </>
   )
