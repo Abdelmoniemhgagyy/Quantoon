d029a2dad5c00de7ad846ddf43d8122c0cfa8aad
Abdelmoniem Ahmed Hgagy <137680518+Abdelmoniemhgagyy@users.noreply.github.com>
2025-07-07 10:46:19 +0300

    update mainPage

--- src/Pages/MainPage/Main.jsx
+++ src/Pages/MainPage/Main.jsx
@@ -1,19 +1,20 @@
-import React from 'react'
-import MainPageCard from '../../components/MainPageCard/MainPageCard'
-import "./main.css"
-import data from "../../data/mainPage"
-import SocialIcon from '../../components/SocialIcon'
-
+import React from 'react';
+import MainPageCard from '../../components/MainPageCard/MainPageCard';
+import "./main.css";
+import data from "../../data/mainPage";
+import SocialIcon from '../../components/SocialIcon';
 
 function Main() {
   return (
-  <div className='mt-[80px] mr-[60px] flex flex-wrap align-center justify-center gap-[50px]'>
-     {data.map((item)=>(
-      <MainPageCard title={item.title} subTitle={item.subTitle} url={item.url}/>
-     ))}
-     <SocialIcon/> 
-  </div>
-  )
+    <div className="mt-[40px] sm:mt-[80px] mr-[60px] md:mr-[80px] px-4 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-5">
+      {data.map((item) => (
+        <MainPageCard key={item.id} title={item.title} url={item.url} icon={item.icon} image={item.image} />
+      ))}
+      <div className="col-span-full flex justify-center mt-6">
+        <SocialIcon />
+      </div>
+    </div>
+  );
 }
 
-export default Main
+export default Main;
--- src/components/MainPageCard/MainPageCard.jsx
+++ src/components/MainPageCard/MainPageCard.jsx
@@ -1,13 +1,27 @@
-import "./main-page-card.css"
-import { useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom';
 
-function MainPageCard({title,subTitle,url}) {
-  const navigate = useNavigate()
+function MainPageCard({ title, url, icon,image }) {
+  const navigate = useNavigate();
+
   return (
-    <div>
-       <div class="card" onClick={()=>navigate(url)} data-text={subTitle}>{title}</div>
+    <div 
+      className="w-full text-[14px] sm:text-[16px]  sm:w-[180px] h-[120px] sm:h-[180px] bg-white shadow-md rounded-2xl flex flex-col 
+                 items-center justify-center text-center text-gray-800 font-bold 
+                 transition-transform duration-300 hover:scale-105 hover:shadow-xl active:scale-95 
+                 cursor-pointer"
+      onClick={() => navigate(url)}
+    >   
+       {image ? (
+        <img src={image} alt={title} className="w-14 h-14 mb-3 object-contain" />
+      ) : (
+        <div className="text-4xl mb-3">{icon}</div>
+      )}
+
+
+
+      <div className="text-md px-2">{title}</div>
     </div>
-  )
+  );
 }
 
-export default MainPageCard
+export default MainPageCard;
