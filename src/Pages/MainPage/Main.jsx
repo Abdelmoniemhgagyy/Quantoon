f47c4d3cbf53ab459bd67aa7275a671dbf709db5
Abdelmoniem Ahmed Hgagy <137680518+Abdelmoniemhgagyy@users.noreply.github.com>
2025-07-07 11:38:38 +0300

    update social components

--- src/Pages/MainPage/Main.jsx
+++ src/Pages/MainPage/Main.jsx
@@ -6,7 +6,7 @@ import SocialIcon from '../../components/SocialIcon';
 
 function Main() {
   return (
-    <div className="mt-[40px] sm:mt-[80px] mr-[60px] md:mr-[80px] px-4 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-5">
+    <div className="mt-[40px] sm:mt-[80px] mr-[60px] md:mr-[80px] px-4 grid grid-cols-2 sm:grid-cols-3  lg:grid-cols-5 gap-5">
       {data.map((item) => (
         <MainPageCard key={item.id} title={item.title} url={item.url} icon={item.icon} image={item.image} />
       ))}
